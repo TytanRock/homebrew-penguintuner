cddcc5bfe3b65e6557504af56134259c98c8f7fb
TytanRock <coryness@tytan.rocks>
2021-07-20 18:45:43 -0400

    More fixes according to actions

--- Formula/penguintuner.rb
+++ Formula/penguintuner.rb
@@ -1,21 +1,20 @@
 class Penguintuner < Formula
   desc "GUI application to interact with CTRE CAN devices"
   homepage "https://penguintuner.tytan.rocks"
-  head "https://github.com/TytanRock/PenguinTuner.git"
   url "https://github.com/TytanRock/PenguinTuner/archive/refs/tags/v0.4.3.tar.gz"
+  head "https://github.com/TytanRock/PenguinTuner.git"
   sha256 "dfa636173ad3a0f3ad1fe6c4aaa1b9c2a4fec926c24aee80fe407a9124a990e4"
   license "BSD-3-Clause"
   
-  depends_on "pkg-config"
   depends_on "cmake"
-  depends_on "python3"
+  depends_on "pkg-config"
   depends_on "curl"
   depends_on "libssh"
   depends_on "gtk+3"
+  depends_on "python3"
 
   def install
     system "./configure.sh"
     system "./build.sh", "install"
   end
 end
-
